401d7544a98c0b9d8349af394e49f92889332d29
Sarita <atanacio.sarita@pucp.pe>
2020-06-05 23:42:43 -0500

    sarita1

--- src/main/java/com/example/exparcialg4/entity/Detalle.java
+++ src/main/java/com/example/exparcialg4/entity/Detalle.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 
 @Entity
 @Table(name = "detalle")
-public class detalle {
+public class Detalle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "iddetalle")

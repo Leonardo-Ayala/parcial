8725a2fcbf8ab49c4dd94bf68c2a9cbdd7c0e68a
Leonardo <a20125980@pucp.pe>
2020-06-06 12:55:08 -0500

    navbar1

---
+++ src/main/java/com/example/exparcialg4/controller/Registrado/RegistradoController.java
@@ -0,0 +1,37 @@
+package com.example.exparcialg4.controller.Registrado;
+
+
+import com.example.exparcialg4.entity.Productos;
+import com.example.exparcialg4.repository.ProductosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/principal")
+public class RegistradoController {
+
+    @Autowired
+    ProductosRepository productosRepository;
+    @GetMapping(value={"","/"})
+    public String listarProductos(Model model){
+        model.addAttribute("listaProductos",productosRepository.findAll());
+        return "Registrado/listarProductos";
+    }
+
+    @GetMapping(value={"/verMas"})
+    public String verMasProductos(Model model,@RequestParam("id") int id){
+        Optional<Productos> opt = productosRepository.findById(id);
+        if(opt.isPresent()){
+            model.addAttribute("productos", opt.get());
+            return "Registrado/verMas";
+        }else{
+            return "redirect:/principal";
+        }
+    }
+}

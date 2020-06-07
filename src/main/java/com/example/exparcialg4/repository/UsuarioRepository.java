87ef56b87b8a582f2623f00721fcbd3f6162949d
ivanmo20 <63992740+ivanmo20@users.noreply.github.com>
2020-06-06 22:20:49 -0500

    admiController

--- src/main/java/com/example/exparcialg4/repository/UsuarioRepository.java
+++ src/main/java/com/example/exparcialg4/repository/UsuarioRepository.java
@@ -5,7 +5,11 @@ import com.example.exparcialg4.entity.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuarios,Integer> {
+
     public Usuarios findByEmail(String email);
+    public List<Usuarios> findByRol (String rol);
 }
